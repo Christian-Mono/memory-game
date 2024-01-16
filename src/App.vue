14f734abc00fe1d7eca2f0910110a2a015ae1f9e
Christian <christian@mono.studio>
2024-01-16 11:49:29 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Board from './components/Board.vue'
+</script>
+
+<template>
+  <div class="container justify-center flex-auto">
+
+    <h1 class="text-2xl text-red"> Ciao mondo</h1>
+    <Board></Board>
+  </div>
+</template>
